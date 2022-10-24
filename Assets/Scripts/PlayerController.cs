298d1b16566e760e2ce4f1f38071dd8589d49c26
TricycleAntler <e.borjesson@live.com>
2022-10-24 10:27:06 +0200

    Jump script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,36 +6,107 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
 
-    public float jumpHeight;
-    NavMeshAgent agent;
+	private Rigidbody rb;
+	private NavMeshAgent agent;
+
+	[SerializeField] private float jumpForce;
+
+	private bool _isGrounded = true;
 
 
 
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
+	void Awake()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		if (agent == null)
+		{
+			gameObject.AddComponent<NavMeshAgent>();
+			agent = GetComponent<NavMeshAgent>();
+		}
+		SetUpRigidbody();
+	}
 
+	void Update()
+	{
 
-    void Update()
-    {
+		if (Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Input.GetMouseButton(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
 
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-                agent.SetDestination(hit.point);
-            }
-        }
+		if (Input.GetMouseButtonDown(1))
+		{
+			TryJump();
+		}
+	}
 
-        if (Input.GetMouseButtonDown(1))
-        {
+	public void TryJump()
+	{
+		if (_isGrounded) return;
+		DoJump();
+	}
 
-            Debug.Log("Jump");
-        }
-    }
+	private void DoJump()
+	{
+		DisableNavMeshAgent();
+		EnableRB();
+		rb.AddRelativeForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
+	}
+
+	private void SetUpRigidbody()
+	{
+		rb = GetComponent<Rigidbody>();
+		if (rb == null)
+		{
+			gameObject.AddComponent<Rigidbody>();
+			rb = GetComponent<Rigidbody>();
+		}
+		DisableRB();
+		rb.freezeRotation = true;
+	}
+
+	// Effectively enables rigidbody.
+	private void EnableRB()
+	{
+		rb.isKinematic = false;
+		rb.useGravity = true;
+	}
+
+	// Effectively disables rigidbody. Technically still enabled but won't do much.
+	private void DisableRB()
+	{
+		rb.isKinematic = false;
+		rb.useGravity = true;
+	}
+
+	private void EnableNavMeshAgent()
+	{
+		agent.updatePosition = true;
+		agent.updateRotation = true;
+		agent.isStopped = false;
+	}
+
+	private void DisableNavMeshAgent()
+	{
+		agent.SetDestination(transform.position); // I believe this will prevent the agent from break if we jump while moving.
+		agent.updatePosition = false;
+		agent.updateRotation = false;
+		agent.isStopped = true;
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.collider != null && other.collider.tag == "Ground" && !_isGrounded)
+		{ // Condition assumes all ground is tagged with "Ground".
+			DisableRB();
+			EnableNavMeshAgent();
+			_isGrounded = true;
+		}
+	}
 }
-
